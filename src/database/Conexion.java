b37339778bb991e6fb03a009cc70e20dff61f95e
edimez14 <edimez14@gmail.com>
2024-10-19 21:43:21 -0500

    update file conexion.java

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -15,10 +15,10 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
     private final String DRIVER="com.mysql.jdbc.Driver";
-    private final String URL="jdbc:mysql://sql10.freesqldatabase.com:3306/";
-    private final String DB="sql10734745";
-    private final String USER="sql10734745";
-    private final String PASSWORD="psktmJVElC";
+    private final String URL="bqkvr2puqldwqoel39uj-mysql.services.clever-cloud.com:3306";
+    private final String DB="bqkvr2puqldwqoel39uj";
+    private final String USER="ua1ug1iu9ymrlzqx";
+    private final String PASSWORD="20KHM6qE5VgoeC8QPCCI";
     
     public Connection cadena;
     public static Conexion instancia;
